c060667d6f2cd6369bd09eb1633005f6bec59585
lakshya sharma <112774859+LAKSHYASHARMA2006GITHUB@users.noreply.github.com>
2025-06-07 22:26:31 +0530

    Update lower_tri.py

--- ASSIGNMENT_WEEK1/lower_tri.py
+++ ASSIGNMENT_WEEK1/lower_tri.py
@@ -1,6 +1,6 @@
 #Lower triangle
 num = int(input("Enter the number of rows"))  # for user input 
 for i in range(0,num +1):
-        for j in range(0,i+1):
+        for j in range(0,i):
             print("*",end = '' '')
         print() 
