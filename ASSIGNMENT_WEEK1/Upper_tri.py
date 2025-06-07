5e88c35ebd27335faf401120275e53a79f3a09a1
lakshya sharma <112774859+LAKSHYASHARMA2006GITHUB@users.noreply.github.com>
2025-06-07 22:26:06 +0530

    Update Upper_tri.py

--- ASSIGNMENT_WEEK1/Upper_tri.py
+++ ASSIGNMENT_WEEK1/Upper_tri.py
@@ -1,6 +1,6 @@
 # upper_triangle using character "*"
 num = int(input("Enter the number of rows :"))  # for user input 
 for i in range(num+1,0,-1):
-        for j in range(0,i):
+        for j in range(0,i-1):
             print("*",end = '' '')
         print() 
